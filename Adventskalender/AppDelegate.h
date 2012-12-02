24ced4a5cda6a61675ef80033f725e1b2ba46d41
Jan Brinkmann <lucky@the-luckyduck.de>
2012-12-02 14:55:44 +0100

    Initial Commit

---
+++ Adventskalender/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventskalender
+//
+//  Created by Jan Brinkmann on 12/2/12.
+//  Copyright (c) 2012 Jan Brinkmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Adventskalender/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Adventskalender
+//
+//  Created by Jan Brinkmann on 12/2/12.
+//  Copyright (c) 2012 Jan Brinkmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
